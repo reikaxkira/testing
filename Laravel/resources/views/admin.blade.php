480212e405267d0777c9d7240a0e164d8b84d6de
DevTeam.Jay <devteam_jay@DevTeam-Jay.local>
2019-03-04 18:10:21 +0800

    debug crud please update

--- Laravel/resources/views/admin.blade.php
+++ Laravel/resources/views/admin.blade.php
@@ -1,23 +1,5 @@
 @extends('layouts.master')
 @section('content')
-@if(Auth::check()) 
-  <div class="row">
-      <div class="col-sm-12">
-         <div class="full-right">
-             <h2> Admin Blog Page </h2>
-         </div>
-      </div>
-  </div>
 
-  <table class="table table-bordered">
-      <tr>
-          <th width="80px">No</th>
-          <th> Body</th>
-          <th width="140px" class="text-center"> 
-              <a href="#" class="btn btn-success btn-sm"></a>
-          </th>
-      </tr>
-
-  </table>
 @endif
 @endsection
